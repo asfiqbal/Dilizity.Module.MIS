92d12210a439850eb3d5bb7c658c6ef61a4d7306
Syed Asif Iqbal <sasifiqbal@gmail.com>
2017-01-07 15:20:02 +0500

    Add Role fixes 2

---
+++ FrontEnd/ReportingMIS/app/controllers/actionRoleController.js
@@ -0,0 +1,142 @@
+﻿
+(function () {
+    'use strict';
+
+    angular
+        .module('ReportingMIS')
+        .controller('actionRoleController', actionRoleController);
+
+    actionRoleController.$inject = ['$scope', '$filter', '$stateParams', '$rootScope', 'AuthenticationService', 'RoleService', 'AlertService', 'HelperService'];
+    function actionRoleController($scope, $filter, $stateParams, $rootScope, AuthenticationService, RoleService, AlertService, HelperService) {
+        var vm = this;
+
+        var roleId = -1;
+
+        vm.title = '';
+
+        vm.addRole = addRole;
+        vm.loadScreenPermissionsAndInfo = loadScreenPermissionsAndInfo;
+        vm.roleName = '';
+        vm.availablePermissions = {};
+        vm.assignedPermissions = [];
+        vm.selectedRolePermissions = [];
+        vm.permissionName = '';
+
+
+        vm.deleteTreeItem = function (tree, id) {
+            for (var i = 0; i < tree.length; i++) {
+                if (tree[i].Id == id) {
+                    tree.splice(i, 1);
+                    i = 0;
+                } else {
+                    if (tree[i].children.length > 0) {
+                        tree[i].children = vm.deleteTreeItem(tree[i].children, id);
+                    }
+                }
+            }
+            return tree;
+        }
+
+        vm.movePermissions = function (sourceTree, targetTree, sourceModel, targetModel) {
+            console.log('movePermissions Begin');
+
+            var sourceId = sourceTree.currentNode.Id;
+            var targetId = targetTree.currentNode.Id;
+
+            //console.log('sourceId', sourceId);
+            //console.log('targetId', targetId);
+
+            var sourceNode = HelperService.deepSearch(sourceModel, 'Id', sourceId);
+            var targetNode = HelperService.deepSearch(targetModel, 'Id', targetId);
+            //console.log('sourceNode', sourceNode);
+            //console.log('targetNode', targetNode);
+
+            vm.deleteTreeItem(sourceModel, sourceId);
+            targetNode.children.push(angular.copy(sourceNode));
+            console.log('movePermissions End');
+        }
+
+        vm.copyPermissions = function (sourceTree, targetTree, sourceModel, targetModel) {
+            console.log('copyPermissions Begin');
+
+            var sourceId = sourceTree.currentNode.Id;
+            var targetId = targetTree.currentNode.Id;
+
+            //console.log('sourceId', sourceId);
+            //console.log('targetId', targetId);
+
+            var sourceNode = HelperService.deepSearch(sourceModel, 'Id', sourceId);
+            var targetNode = HelperService.deepSearch(targetModel, 'Id', targetId);
+            //console.log('sourceNode', sourceNode);
+            //console.log('targetNode', targetNode);
+
+            targetNode.children.push(angular.copy(sourceNode));
+            console.log('copyPermissions End');
+        }
+
+        vm.RemovePermissions = function (targetTree, targetModel) {
+            console.log('RemovePermissions Begin');
+
+            var targetId = targetTree.currentNode.Id;
+
+            //console.log('sourceId', sourceId);
+            //console.log('targetId', targetId);
+
+            var targetNode = HelperService.deepSearch(targetModel, 'Id', targetId);
+            //console.log('sourceNode', sourceNode);
+            //console.log('targetNode', targetNode);
+
+            vm.deleteTreeItem(targetModel, targetId);
+            console.log('RemovePermissions End');
+        }
+
+        function loadScreenPermissionsAndInfo() {
+            console.log("loadScreenPermissionsAndInfo Begin");
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            RoleService.GetActionRoleScreenInfo(vm.permissionName, tmpUserName, roleId, function (response) {
+                var data = angular.fromJson(response.data);
+                vm.availablePermissions = data.AvailablePermissions;
+                vm.assignedPermissions = data.AssignedPermissions;
+                console.log("data", data);
+            }, function (response) {
+                AlertService.Add("Error", "You don't have permission");
+            });
+            console.log("loadScreenPermissionsAndInfo End");
+        };
+
+
+        (function initController() {
+            console.log("actionRoleController.initController -> Begin");
+            console.log("$stateParams.roleId", $stateParams.roleId);
+            console.log("$stateParams.permissionName", $stateParams.permissionName);
+
+            roleId = $stateParams.roleId;
+            vm.permissionName = $stateParams.permissionName;
+            var arr = vm.permissionName.split(".");
+            vm.title = arr[arr.length - 1] + ' Role';
+            console.log("vm.title", vm.title);
+            //console.log("permissionName", vm.permissionName);
+
+            loadScreenPermissionsAndInfo();
+   
+            console.log("actionRoleController.initController -> End");
+        })();
+
+        function addRole() {
+            console.log("addRole Begin");
+            //vm.dataLoading = true;
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            RoleService.AddRole(permissionName, tmpUserName, 1, function (response) {
+                vm.MetaReport = response.data;
+                vm.reportId = vm.MetaReport.ReportId;
+                vm.permissionName = vm.MetaReport.PermissionName;
+                console.log("response.data", response.data);
+                console.log("vm.MetaReport.DisplayName", vm.MetaReport.DisplayName);
+                console.log("loadReport Called");
+                vm.fields = vm.MetaReport.fieldCollection;
+            });
+            console.log("addRole End");
+        };
+    }
+
+})();
---
+++ FrontEnd/ReportingMIS/app/services/HelperService.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('ReportingMIS')
+        .factory('HelperService', HelperService);
+
+    HelperService.$inject = ['$rootScope'];
+    function HelperService($rootScope) {
+        var HelperService;
+        return HelperService = {
+            deepSearch: function deepSearch(theObject, key, value) {
+                var result = null;
+                if (theObject instanceof Array) {
+                    for (var i = 0; i < theObject.length; i++) {
+                        result = deepSearch(theObject[i], key, value);
+                        if (result != null)
+                            return result;
+                    }
+                }
+                else {
+                    for (var prop in theObject) {
+                        //console.log(prop + ': ' + theObject[prop]);
+                        if (theObject.hasOwnProperty(prop)) {
+                            if (prop == key) {
+                                if (theObject[prop] == value) {
+                                    console.log('Object Found', value);
+                                    return theObject;
+                                }
+                            }
+                        }
+
+                        if (theObject[prop] instanceof Object || theObject[prop] instanceof Array)
+                            result = deepSearch(theObject[prop], key, value);
+                        if (result != null)
+                            return result;
+
+                    }
+                }
+                return result;
+            }
+        };
+    };
+})();
