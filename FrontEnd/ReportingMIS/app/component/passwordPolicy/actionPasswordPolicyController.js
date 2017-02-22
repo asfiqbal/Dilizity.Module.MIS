06960bd97ef7954c9b0577f2e196af0100b89270
Syed Asif Iqbal <sasifiqbal@gmail.com>
2017-02-22 21:27:04 +0500

    Password Policy Search and Action

---
+++ FrontEnd/ReportingMIS/app/component/passwordPolicy/actionPasswordPolicyController.js
@@ -0,0 +1,358 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('ReportingMIS')
+        .controller('actionUserController', actionUserController);
+
+    actionUserController.$inject = ['$uibModal', '$window', '$scope', '$stateParams', '$rootScope', 'AuthenticationService', 'Notification', 'HelperService', 'UniversalService', 'CommunicationService'];
+    function actionUserController($uibModal, $window, $scope, $stateParams, $rootScope, AuthenticationService, Notification, HelperService, UniversalService, CommunicationService) {
+        var vm = this;
+
+        var roleId = -1;
+        var makerId = -1;
+
+        vm.title = '';
+        vm.newMessage = '';
+
+
+        vm.doAction = doAction;
+        vm.isMakerCheckerMode = isMakerCheckerMode;
+        vm.isMakerMode = isMakerMode;
+        vm.isCheckerMode = isCheckerMode;
+
+        vm.loadScreenPermissionsAndInfo = loadScreenPermissionsAndInfo;
+        vm.roleName = '';
+        vm.availablePermissions = {};
+        vm.assignedPermissions = [];
+        vm.userPermission = {};
+        vm.notes = [];
+
+        vm.permissionName = '';
+
+        vm.deleteTreeItem = function (tree, id) {
+            for (var i = 0; i < tree.length; i++) {
+                if (tree[i].Id == id) {
+                    tree.splice(i, 1);
+                    i = 0;
+                } else {
+                    if (tree[i].children.length > 0) {
+                        tree[i].children = vm.deleteTreeItem(tree[i].children, id);
+                    }
+                }
+            }
+            return tree;
+        }
+
+        vm.movePermissions = function (sourceTree, targetTree, sourceModel, targetModel) {
+            console.log('movePermissions Begin');
+
+            var sourceId = sourceTree.currentNode.Id;
+            var targetId = targetTree.currentNode.Id;
+
+            var sourceNode = HelperService.deepSearch(sourceModel, 'Id', sourceId);
+            var targetNode = HelperService.deepSearch(targetModel, 'Id', targetId);
+
+            vm.deleteTreeItem(sourceModel, sourceId);
+            targetNode.children.push(angular.copy(sourceNode));
+            console.log('movePermissions End');
+        }
+
+        vm.copyPermissions = function (sourceTree, targetTree, sourceModel, targetModel) {
+            console.log('copyPermissions Begin');
+
+            var sourceId = sourceTree.currentNode.Id;
+            var targetId = targetTree.currentNode.Id;
+
+            //console.log('sourceId', sourceId);
+            //console.log('targetId', targetId);
+
+            var sourceNode = HelperService.deepSearch(sourceModel, 'Id', sourceId);
+            var targetNode = HelperService.deepSearch(targetModel, 'Id', targetId);
+            //console.log('sourceNode', sourceNode);
+            //console.log('targetNode', targetNode);
+
+            targetNode.children.push(angular.copy(sourceNode));
+            console.log('copyPermissions End');
+        }
+
+        vm.RemovePermissions = function (targetTree, targetModel) {
+            console.log('RemovePermissions Begin');
+
+            var targetId = targetTree.currentNode.Id;
+
+            //console.log('sourceId', sourceId);
+            //console.log('targetId', targetId);
+
+            var targetNode = HelperService.deepSearch(targetModel, 'Id', targetId);
+            //console.log('sourceNode', sourceNode);
+            //console.log('targetNode', targetNode);
+
+            vm.deleteTreeItem(targetModel, targetId);
+            console.log('RemovePermissions End');
+        }
+
+        function loadScreenPermissionsAndInfo() {
+            console.log("loadScreenPermissionsAndInfo Begin");
+            var tmpUserName = $rootScope.globals.currentUser.username;
+
+
+            var model = {
+                RoleId: roleId,
+                MakerId: makerId
+            }
+
+            UniversalService.Do(vm.permissionName, tmpUserName, model,
+                function (response) {
+                    console.log("Success!");
+                    var data = angular.fromJson(response.data);
+
+                    if (response.data.ErrorCode == 0) {
+                        roleId = data.Data.Id;
+                        vm.roleName = data.Data.Name;
+                        vm.availablePermissions = data.Data.AvailablePermissions;
+                        vm.assignedPermissions = data.Data.AssignedPermissions;
+                        vm.userPermission = data.Data.UserPermission;
+                        vm.notes = data.Data.Notes;
+                    }
+                    else {
+                        Notification.error({ message: response.data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                    }
+                },
+                function (response) {
+                    console.log("response!", response);
+                    Notification.error({ message: response.statusText, positionY: 'bottom', positionX: 'right' });
+                }
+            );
+
+            console.log("loadScreenPermissionsAndInfo End");
+        };
+
+        (function initController() {
+            console.log("actionUserController.initController -> Begin");
+            console.log("$stateParams.roleId", $stateParams.roleId);
+            console.log("$stateParams.permissionName", $stateParams.permissionName);
+
+            roleId = parseInt($stateParams.roleId);
+            if (!HelperService.IsNumber(roleId)) {
+                roleId = 0;
+            }
+            console.log("roleId", roleId);
+
+            vm.permissionName = $stateParams.permissionName;
+            makerId = parseInt($stateParams.makerId);
+            console.log("makerId", makerId);
+            if (!HelperService.IsNumber(makerId)) {
+                makerId = 0;
+            }
+            console.log("makerId", makerId);
+
+            var arr = vm.permissionName.split(".");
+            vm.title = arr[arr.length - 1] + ' Role';
+            console.log("vm.title", vm.title);
+
+
+            loadScreenPermissionsAndInfo();
+   
+            console.log("actionUserController.initController -> End");
+        })();
+
+        function doAction(actionStats, additionalPermission, form) {
+            console.log("doAction Begin");
+
+            if (!validateForm(form)) {
+                return false;
+            }
+
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            var actionPermission = vm.permissionName + additionalPermission;
+
+            var model = CreateModel(actionStats);
+
+            UniversalService.Do(actionPermission, tmpUserName, model,
+                function (response) {
+                    console.log("Success!");
+                    var data = angular.fromJson(response.data);
+
+                    if (data.ErrorCode == 0) {
+                        Notification.success({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                    }
+                    else {
+                        Notification.error({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                    }
+                },
+                function (response) {
+                    console.log("response!", response);
+                    Notification.error({ message: response.statusText, positionY: 'bottom', positionX: 'right' });
+                }
+            );
+
+            console.log("doAction End");
+        }
+
+        function CreateModel(makerStatus) {
+            console.log("CreateModel Begin");
+            //vm.dataLoading = true;
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            var model = {};
+
+            if (isMakerCheckerMode()) {
+                model = {
+                    "Id": roleId,
+                    "MakerId": makerId,
+                    "Name": vm.roleName,
+                    "Status": makerStatus,
+                    "AvailablePermissions": vm.availablePermissions,
+                    "AssignedPermissions": vm.assignedPermissions,
+                    "Notes": vm.notes
+                };
+            }
+            else {
+
+                model = {
+                    "Id": roleId,
+                    "Name": vm.roleName,
+                    "AvailablePermissions": vm.availablePermissions,
+                    "AssignedPermissions": vm.assignedPermissions
+                };
+            }
+
+            console.log("model", model);
+
+
+            return model;
+            console.log("CreateModel End");
+        };
+
+        function isMakerCheckerMode() {
+            return HelperService.containsAny(vm.permissionName, ['Maker', 'Checker'])
+        };
+
+        function isMakerMode() {
+            return HelperService.containsAny(vm.permissionName, ['Maker'])
+        };
+
+        function isCheckerMode() {
+            return HelperService.containsAny(vm.permissionName, ['Checker'])
+        };
+
+        function validateForm(form) {
+            console.log("form", form);
+            console.log("form.$valid", form.$valid);
+            console.log("vm.assignedPermissions.length", vm.assignedPermissions.length);
+
+            if (!form.$valid) {
+                if (!vm.roleName)  {
+                    Notification.error({ message: 'Role Name is Required', positionY: 'bottom', positionX: 'right' });
+                }
+                if (vm.assignedPermissions.length < 2) {
+                    Notification.error({ message: 'Permissions are not correctly defined', positionY: 'bottom', positionX: 'right' });
+                }
+
+                return false;
+            }
+
+            if (!form.$dirty) {
+                Notification.error({ message: 'No change has been made', positionY: 'bottom', positionX: 'right' });
+                return false;
+            }
+            return true;
+        };
+
+        vm.saveNote = function saveNote() {
+            console.log("saveNote Begin");
+
+            console.log("vm.notes", vm.notes);
+            console.log("vm.newMessage", vm.newMessage);
+
+            if (!vm.notes) {
+                vm.notes = new Array();
+            }
+
+            console.log("vm.notes", vm.notes);
+
+            vm.notes.push(vm.newMessage);
+            vm.newMessage = '';
+
+            console.log("saveNote End");
+        };
+
+        vm.compare = function (size, parentSelector) {
+            var parentElem = parentSelector ?
+              angular.element($document[0].querySelector('.x_panel' + parentSelector)) : undefined;
+
+            var model = {
+                RoleId: roleId,
+                MakerId: makerId
+            };
+
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            var outHtml = '';
+
+            var jsondiffpatch = $window.jsondiffpatch;
+            var jsonDiff = jsondiffpatch.create({
+                objectHash: function (obj) {
+                    if (typeof obj._id !== 'undefined') {
+                        return obj._id;
+                    }
+                    if (typeof obj.id !== 'undefined') {
+                        return obj._id;
+                    }
+                    if (typeof obj.name !== 'undefined') {
+                        return obj.name;
+                    }
+                    return JSON.stringify(obj);
+                },
+                arrays: {
+                    detectMove: true,
+                    includeValueOnMove: false
+                },
+                textDiff: {
+                    minLength: 60
+                }
+            });
+
+            UniversalService.Do(vm.permissionName + '.Compare', tmpUserName, model,
+               function (response) {
+                   var data = angular.fromJson(response.data);
+
+                   if (data.ErrorCode == 0) {
+                       console.log("Success!");
+                       var delta = jsondiffpatch.diff(data.Data.Old, data.Data.New);
+                       outHtml = jsondiffpatch.formatters.html.format(delta, data.Data.Old);
+                       CommunicationService.Set('COMPARE', outHtml);
+                       //console.log('outHtml', outHtml);
+
+                       var modalInstance = $uibModal.open({
+                           animation: true,
+                           ariaLabelledBy: 'modal-title',
+                           ariaDescribedBy: 'modal-body',
+                           templateUrl: 'app/templates/compareJSON.html',
+                           controller: 'compareJSONController',
+                           controllerAs: 'vm',
+                           windowClass: 'zindex',
+                           size: size,
+                           appendTo: parentElem,
+                           resolve: {
+                               beautyHtml: function () {
+                                   return outHtml;
+                               }
+                           }
+                       });
+                   }
+                   else {
+                       Notification.error({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                   }
+               },
+               function (response) {
+                   console.log("response!", response);
+                   Notification.error({ message: response.statusText, positionY: 'bottom', positionX: 'right' });
+               }
+            );
+
+
+
+        }
+    }
+
+})();
---
+++ FrontEnd/ReportingMIS/app/component/passwordPolicy/searchPasswordPolicyController.js
@@ -0,0 +1,248 @@
+﻿
+(function () {
+    'use strict';
+
+    angular
+        .module('ReportingMIS')
+        .controller('searchPasswordPolicyController', searchPasswordPolicyController);
+
+    searchPasswordPolicyController.$inject = ['$scope', '$stateParams', '$rootScope', 'AuthenticationService', 'UniversalService', 'Notification'];
+    function searchPasswordPolicyController($scope, $stateParams, $rootScope, AuthenticationService, UniversalService, Notification) {
+        var vm = this;
+
+        vm.slide = false;
+        vm.search = search;
+        vm.loadScreenPermissionsAndInfo = loadScreenPermissionsAndInfo;
+        vm.load = load;
+        vm.delete = deletes;
+        vm.isMakerDelete = isMakerDelete;
+
+        vm.id = '';
+        vm.name = '';
+        vm.permissionId = '';
+        vm.userPermission = {};
+        vm.selectedPasswordPolicy = [];
+        
+        $scope.sort = { fieldName: "Id", order: "asc" };
+
+        $scope.pagination = {
+            pageSize: 15,
+            pageNumber: 1,
+            totalItems: null,
+            getTotalPages: function () {
+                //console.log("getTotalPages Begin");
+                return Math.ceil(this.pageNumber * this.pageSize);
+            },
+            nextPage: function () {
+                console.log("nextPage Begin");
+                //if (this.pageNumber < this.getTotalPages()) {
+                    this.pageNumber++;
+                    load();
+                //}
+                console.log("nextPage End");
+            },
+            previousPage: function () {
+                console.log("previousPage Begin");
+                if (this.pageNumber > 1) {
+                    this.pageNumber--;
+                    load();
+                }
+                console.log("previousPage End");
+            }
+        };
+
+        $scope.gridOptions = {
+            enableGridMenu: true,
+            enableSelectAll: true,
+            multiSelect: true,
+            enablePaginationControls: false,
+            useExternalPagination: true,
+            useExternalSorting: true,
+            columnDefs:[ 
+              {name:'Id',field:'Id'},
+              {name:'Name',field:'Name'},
+              {name:'Login Id', field: 'LoginId' },
+              {name:'Mobile Number', field: 'MobileNumber' },
+              {name:'Email', field: 'Email' },
+              {name:'Account Locked', field: 'AccountLocked' },
+              {name:'Updated By', field: 'UpdatedBy' },
+              {name:'Updated On', field: 'UpdatedOn' },
+              {name:'Action', cellTemplate: '<div>'+
+                                                '<ul class="nav navbar-right panel_toolbox">'+
+	                                                '<li>'+
+                                                		'<a ng-show="grid.appScope.vm.userPermission.View===\'Dilizity.Backoffice.PasswordPolicy.View\'" uib-tooltip="View Password Policy" tooltip-placement="left" ui-sref="index.ActionPasswordPolicy({ id: row.entity.Id, makerId: 0, permissionName: \'Dilizity.Backoffice.PasswordPolicy.View\' })" class="pull-right"><i class="fa fa-eye"></i></a>' +
+	                                                '</li>'+
+	                                                '<li>'+
+		                                                '<a ng-show="grid.appScope.vm.userPermission.Edit===\'Dilizity.Backoffice.PasswordPolicy.Edit\'" uib-tooltip="Edit Password Policy" tooltip-placement="left" ui-sref="index.ActionPasswordPolicy({ id: row.entity.Id, makerId: 0, permissionName: \'Dilizity.Backoffice.PasswordPolicy.Edit\' })" class="pull-right"><i class="fa fa-pencil-square"></i></a>' +
+		                                                '<a ng-show="grid.appScope.vm.userPermission.Edit===\'Dilizity.Backoffice.PasswordPolicy.Edit.Maker\'" uib-tooltip="Edit Password Policy [Maker]" tooltip-placement="left" ui-sref="index.ActionPasswordPolicy({ id: row.entity.UserId, makerId: 0, permissionName: \'Dilizity.Backoffice.PasswordPolicy.Edit.Maker\' })" class="pull-right"><i class="fa fa-pencil-square"></i></a>' +
+	                                                '<li>' +
+	                                                '<li>' +
+		                                                '<a ng-show="grid.appScope.vm.userPermission.Delete===\'Dilizity.Backoffice.PasswordPolicy.Delete\'" uib-tooltip="Delete Password Policy" tooltip-placement="left" ui-sref="index.ActionPasswordPolicy({ id: row.entity.Id, makerId: 0, permissionName: \'Dilizity.Backoffice.PasswordPolicy.Delete\' })" class="pull-right"><i class="fa fa-trash"></i></a>' +
+		                                                '<a ng-show="grid.appScope.vm.userPermission.Delete===\'Dilizity.Backoffice.PasswordPolicy.Delete.Maker\'" uib-tooltip="Delete Password Policy [Maker]" tooltip-placement="left" ui-sref="index.ActionPasswordPolicy({ id: row.entity.Id, makerId: 0, permissionName: \'Dilizity.Backoffice.PasswordPolicy.Delete.Maker\' })" class="pull-right"><i class="fa fa-trash"></i></a>' +
+	                                                '<li>' +
+                                                '</ul>' +
+                                               '</div>' }
+            ],
+            data:[],
+            exporterCsvFilename: 'PasswordPolicy.csv',
+            onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+            $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
+                    console.log("sortChanged Begin");
+                    $scope.sort = {};
+                    angular.forEach(sortColumns, function (sortColumn) {
+                        $scope.sort = {
+                            fieldName: sortColumn.name,
+                            order: sortColumn.sort.direction
+                        };
+                    });
+                    console.log("sort", $scope.sort);
+                    load();
+                    console.log("sortChanged End");
+
+            });
+            $scope.gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                var msg = 'row selected ' + row.isSelected;
+                if (row.isSelected){
+                    vm.selectedPasswordPolicy.push(row.entity.UserId);
+                }
+                else {
+                    var pos = vm.selectedPasswordPolicy.indexOf(row.entity.UserId);
+                    vm.selectedPasswordPolicy.splice(pos, 1);
+                    //console.log(row.entity.RoleId);
+                }
+            });
+
+            $scope.gridApi.selection.on.rowSelectionChangedBatch($scope, function (rows) {
+                var msg = 'rows changed ' + rows.length;
+                console.log(msg);
+            });
+            }
+        };
+
+        //$scope.gridOptions.multiSelect = true;
+
+        //$scope.editClickHandler = function (val) {
+        //    console.log(val);
+        //    alert('Name: ' + val);
+        //};
+
+
+        function load() {
+            console.log("load Begin");
+            searchUser();
+            console.log("load End");
+        };
+
+        function loadScreenPermissionsAndInfo() {
+            console.log("loadScreenPermissionsAndInfo Begin");
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            
+            UniversalService.Do(vm.permissionId, tmpUserName, null,
+                  function (response) {
+                      var data = angular.fromJson(response.data);
+
+                      if (data.ErrorCode == 0) {
+                          vm.userPermission = data.Data.UserPermission;
+                      }
+                      else {
+                          Notification.error({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                      }
+                  }, function (response) {
+                      Notification.error({ message: response.statusText, positionY: 'bottom', positionX: 'right' });
+                  });
+            console.log("loadScreenPermissionsAndInfo End");
+        };
+
+
+        (function initController() {
+            console.log("roleController.initController -> Begin");
+            console.log("$stateParams", $stateParams);
+            vm.permissionId = $stateParams.permissionId;
+            loadScreenPermissionsAndInfo();
+            console.log("roleController.initController -> End");
+        })();
+
+        function search() {
+            console.log("searchUser Begin");
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            var searchPermissionId = vm.permissionId + ".Search";
+
+            var model = {
+                Id: vm.id,
+                Name: vm.name,
+                PageSize: $scope.pagination.pageSize,
+                PageNumber: $scope.pagination.pageNumber,
+                Sort: $scope.sort
+            }
+
+            UniversalService.Do(searchPermissionId, tmpUserName, model,
+                function (response) {
+                    console.log("Success!");
+
+                    var data = angular.fromJson(response.data);
+
+                    if (data.ErrorCode == 0) {
+                        $scope.gridOptions.data = data.Data;
+                        $scope.pagination.totalItems = 25;
+                    }
+                    else
+                    {
+                        Notification.error({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                    }
+                },
+                function (response) {
+                    console.log("response!", response);
+                    Notification.error({ message: response.statusText, positionY: 'bottom', positionX: 'right' });
+                }
+            );
+            console.log("searchUser End");
+        };
+
+        function deletes() {
+            console.log("deleteUsers Begin");
+            angular.element('#modal').modal('hide');
+            var tmpUserName = $rootScope.globals.currentUser.username;
+            //Notification.error({ message: 'Error Bottom Right', positionY: 'bottom', positionX: 'right' });
+            var deletePermission = vm.permissionId + ".Delete.Bulk";
+            console.log("permissionId", deletePermission);
+            console.log("vm.selectedPasswordPolicy", vm.selectedPasswordPolicy);
+
+            var model = {
+                Users: vm.selectedPasswordPolicy
+            }
+
+            UniversalService.Do(deletePermission, tmpUserName, model,
+                function (response) {
+                    console.log("Success!");
+                    var data = angular.fromJson(response.data);
+
+                    if (data.ErrorCode == 0) {
+                        Notification.success({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                        vm.selectedPasswordPolicy = [];
+                        searchUser();
+                    }
+                    else {
+                        Notification.error({ message: data.ErrorMessage, positionY: 'bottom', positionX: 'right' });
+                        vm.selectedPasswordPolicy = [];
+                    }
+                },
+                function (response) {
+                    console.log("response!", response);
+                    Notification.error({ message: response.statusText, positionY: 'bottom', positionX: 'right' });
+                }
+            );
+            console.log("deleteUsers End");
+
+        };
+
+
+        function isMakerDelete() {
+            console.log("isMakerDelete Begin");
+            return HelperService.containsAny(vm.userPermission, ['Maker', 'Delete'])
+        };
+
+
+    }
+
+})();
